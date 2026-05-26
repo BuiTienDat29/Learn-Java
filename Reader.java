76f22e706914a4442390f225f954adb357a67719
B.Tien Dat <tiendat294204@gmail.com>
2026-05-26 10:39:38 +0700

    Add files via upload

---
+++ Reader.java
@@ -0,0 +1,27 @@
+public class Reader {
+
+    private int id;
+    private String name;
+    private String phone;
+
+    public Reader(int id,
+                  String name,
+                  String phone) {
+
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+
+        return id + " - "
+                + name + " - "
+                + phone;
+    }
+}
